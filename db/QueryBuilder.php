3ec8b808642392de36ee7523ff5f0ba5835b815e
unknown <dikunbrain@gmail.com>
2021-01-19 10:21:53 +0200

    Add rout to add page

--- db/QueryBuilder.php
+++ db/QueryBuilder.php
@@ -14,9 +14,30 @@ class QueryBuilder
     public function items()
     {
         $stmt = $this->pdo->prepare(
-            "SELECT i.*, dvd.size dvd_size, book.weight book_weight, f.width furniture_width, f.height furniture_height, f.depth furniture_depth FROM items i LEFT JOIN type_dvd dvd ON dvd.item_sku = i.sku LEFT JOIN type_book book ON book.item_sku = i.sku LEFT JOIN type_furniture f ON f.item_sku = i.sku"
+            "SELECT i.*, dvd.size dvd_size, book.weight book_weight, f.dimension furniture_dimension FROM items i LEFT JOIN type_dvd dvd ON dvd.item_sku = i.sku LEFT JOIN type_book book ON book.item_sku = i.sku LEFT JOIN type_furniture f ON f.item_sku = i.sku"
         );
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_CLASS);
+    }
+
+    //insert new item
+    public function insert_item($sku, $name, $price, $type, $specific)
+    {
+        $sql_item = ("INSERT INTO items(sku, name, price, type) 
+        VALUES ({$sku}, '{$name}', {$price}, '{$type}')");
+
+        $sql_type = ("INSERT INTO type_{$type} VALUES ({$sku}, '{$specific}')");
+        try {
+            $stmt = $this->pdo->prepare($sql_item);
+            $stmt->execute();
+            try {
+                $stmt = $this->pdo->prepare($sql_type);
+                $stmt->execute();
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
     }
 }
--- routes/routes.php
+++ routes/routes.php
@@ -7,6 +7,8 @@ $router->get("", 'ItemController@list');
 $router->get('index', 'ItemController@list');
 
 //page where add item
-$router->get('new_item', 'ItemController@new');
+$router->get('new_item', 'ItemController@new_item');
 //add new item
-$router->post('add_item', 'ItemController@add');
+$router->post('add_item', 'ItemController@add_item');
+//delete item
+$router->post('mass_delete', 'ItemController@delete_item');
