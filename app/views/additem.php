cbbfdb74d344e012529ee74a5cafc120ac395912
unknown <dikunbrain@gmail.com>
2021-01-19 12:49:13 +0200

    Display fix

--- app/views/additem.php
+++ app/views/additem.php
@@ -21,25 +21,25 @@ require 'app/views/partials/head.php';
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">SKU</span>
                 </div>
-                <input type="text" class="form-control" placeholder="" name="sku">
+                <input type="text" class="form-control" placeholder="" name="sku" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Name</span>
                 </div>
-                <input type="text" class="form-control" placeholder="" name="name">
+                <input type="text" class="form-control" placeholder="" name="name" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="inputGroup-sizing-default">Price ($)</span>
                 </div>
-                <input type="text" class="form-control" placeholder="" name="price">
+                <input type="text" class="form-control" placeholder="" name="price" required>
             </div>
             <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="basic-addon1">Type Switcher</span>
                 </div>
-                <select class="custom-select" id="inputGroupSelect01" name="type">
+                <select class="custom-select" name="type" id="type_choice" onclick="product_choice()">
                     <option selected>Choose...</option>
                     <option value="dvd">DVD</option>
                     <option value="furniture">Furniture</option>
@@ -47,32 +47,32 @@ require 'app/views/partials/head.php';
                 </select>
             </div>
             <!-- DVD SIZE -->
-            <div class="input-group mb-3">
+            <div class="input-group mb-3" id="dvd_block" style="display: none;">
                 <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Size (MB)</span>
+                    <span class="input-group-text">Size (MB)</span>
                 </div>
-                <input type="text" class="form-control" placeholder="Provide size in MB" name="specific">
+                <input id="dvd" type="text" class="form-control" placeholder="Provide size in MB" name="specific" disabled="true" required>
             </div>
             <!-- FURNITURE SIZE -->
-            <div class="input-group mb-3">
+            <div class="input-group mb-3" id="furniture_block" style="display: none;">
                 <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Dimension (CM)</span>
+                    <span class="input-group-text">Dimension (CM)</span>
                 </div>
-                <input type="text" class="form-control" placeholder="Provide dimension in HxWxL format" name="specific" disabled>
+                <input id="furniture" type="text" class="form-control" placeholder="Provide dimension in HxWxL format" name="specific" disabled="true" required>
             </div>
             <!-- BOOK WEIGHT -->
-            <div class="input-group mb-3">
+            <div class="input-group mb-3" id="book_block" style="display: none;">
                 <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-default">Weight (KG)</span>
+                    <span class="input-group-text">Weight (KG)</span>
                 </div>
-                <input type="text" class="form-control" placeholder="Provide weight in KG" name="specific" disabled>
+                <input id="book" type="text" class="form-control" placeholder="Provide weight in KG" name="specific" disabled="true" required>
             </div>
         </form>
     </div>
 </div>
 
 
-
+<script src="assets/js/addpagechoice.js"></script> 
 <?php
 require 'app/views/partials/footer.php';
 ?>
